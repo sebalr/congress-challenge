df3ea1543297188b92668d0b2dc2a04095365752
sebalr <larrieu.sebastian@gmail.com>
2020-10-17 12:07:01 -0300

    Dashboard module
    Dashbord module created and added to main layout.
    Routing for dashboard module.
    Fix some imports to be full path.

--- src/app/dashboard/dashboard-routing.module.ts
+++ src/app/dashboard/dashboard-routing.module.ts
@@ -1,16 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
+import { DashboardComponent } from './dashboard.component';
 const routes: Routes = [{
   path: '',
-  loadChildren: () => import('./dashboard/dashboard.module').then(m => m.DashboardModule)
+  component: DashboardComponent
 }
 ];
 
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes)
+    RouterModule.forChild(routes)
   ],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class DashboardRoutingModule { }
