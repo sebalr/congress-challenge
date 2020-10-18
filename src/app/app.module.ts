47595c029db8d78f4158c5b36c7e4a692811b894
sebalr <larrieu.sebastian@gmail.com>
2020-10-18 11:57:53 -0300

    Logger interceptor
    Logger interceptor created with tap action to log request and response

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from 'src/app/app-routing.module';
 import { AppComponent } from 'src/app/app.component';
 import { CoreModule } from 'src/app/core/core.module';
 import { AuthInterceptor } from 'src/app/core/interceptors/auth.interceptor';
+import { LoggerInterceptor } from 'src/app/core/interceptors/logger.interceptor';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { AuthInterceptor } from 'src/app/core/interceptors/auth.interceptor';
     CoreModule,
   ],
   providers: [
-    { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true }
+    { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
+    { provide: HTTP_INTERCEPTORS, useClass: LoggerInterceptor, multi: true }
   ],
   bootstrap: [AppComponent]
 })
